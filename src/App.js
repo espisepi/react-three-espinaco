eb1b953e9e1fc3e5531376a8faa3b0e09fb103f6
espisepi <sepinaco@gmail.com>
2020-10-23 23:39:43 +0200

    mapa generado correctamente

--- src/App.js
+++ src/App.js
@@ -7,15 +7,59 @@ import { apply as applySpring, useSpring, a, interpolate } from 'react-spring/th
 
 import data from './data';
 
-import { Plane, OrbitControls } from 'drei';
+import { Box, OrbitControls } from 'drei';
 
+function getMeshFromMapaValue(value) {
+	switch (value) {
+		case 0:
+			return null;
+		case 1:
+			return new THREE.Mesh(
+				new THREE.BoxBufferGeometry(1,1,1),
+				new THREE.MeshBasicMaterial({color:0xff0000})
+			);
+
+	}
+}
+
+
+function LoadScene({}) {
+
+	// Creamos el mapa
+	const mapa = [
+		[1,1,1,1,1],
+		[1,0,0,0,1],
+		[1,0,1,0,1],
+		[1,0,0,0,1],
+		[1,1,1,1,1],
+	];
+
+	// Obtenemos la escena
+	const {scene} = useThree();
+
+	for(let y = 0; y < 5; y++) {
+		for(let x = 0; x < 5; x++) {
+			let value = mapa[y][x];
+			let mesh = getMeshFromMapaValue(value);
+			if(mesh){
+				mesh.position.set(x+2,0,y+2);
+				scene.add(mesh);
+			}
+		}
+	}
+	
+
+
+	return null;
+}
 
 function Scene({ }) {
 
 	return (
 		<>
+		<LoadScene />
 		<a.spotLight intensity={1.2} color="white" position={[0,0,0]} />
-		<Plane args={[2,2]} />
+		<Box args={[2,2,2]} />
 		</>
 	);
 }
