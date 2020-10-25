2121b29b0343cf53aff38e50e2c421055e041b94
espisepi <sepinaco@gmail.com>
2020-10-25 02:05:19 +0200

    new way to instanciate a map (with simple array)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import { Cube } from './components/Cube';
 import useCreatorMap from './hooks/useCreatorMap';
 import simpleReducer from './reducers/simpleReducer';
 
+function splitterArray (array, len) {
+	const res = [];
+	const arrayLength = array.length;
+	for(let i = 0; i < arrayLength; i += len ) {
+		res.push(array.slice(i, i + len ));
+	}
+	console.log(res);
+	return res;
+}
+
 function LoadScene({}) {
 
 	// Creamos el mapa
@@ -20,14 +30,22 @@ function LoadScene({}) {
 		[1,1,0,1,1],
 	];
 
-	const meshesTest = useCreatorMap(mapa, simpleReducer);
+	const mapaTest = [
+		1,1,1,1,1,
+		1,0,0,0,1,
+		1,0,0,0,1,
+		1,1,0,1,1
+	];
+	const mapaTestProcesado = splitterArray(mapaTest,5);
+
+	const meshesTest = useCreatorMap(mapaTestProcesado, simpleReducer);
 
 	return meshesTest;
 }
 
 function Scene({ }) {
-	const array = new Array(16).fill(5);
-	console.log(array)
+	// const array = new Array(16).fill(5);
+	// console.log(array)
 	return (
 		<>
 		<ambientLight intensity={0.3} />
@@ -36,13 +54,12 @@ function Scene({ }) {
 			<LoadScene />
 			<Ground position={[0,-1,0]} />
 			<Player />
-			{array.forEach((el,index)=>(
+			{/* {array.forEach((el,index)=>(
 				<Cube key={index}/>
-			))}
+			))} */}
 		
 		</Physics>
 		<PointerLockControls />
-		{/* <OrbitControls /> */}
 		</>
 	);
 }
