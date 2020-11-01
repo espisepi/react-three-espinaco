36d5e9c3aab5d53365f42464c0f5772609d54e07
espisepi <sepinaco@gmail.com>
2020-11-01 02:10:41 +0100

    created Curve component

--- src/scenes/App2/App2.js
+++ src/scenes/App2/App2.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Canvas } from 'react-three-fiber';
 import { OrbitControls } from 'drei';
 import Loading from '../../components/Loading';
 
+import * as THREE from 'three';
+
+function Curve(props) {
+
+    
+    //Create a closed wavey loop
+    const curve = new THREE.CatmullRomCurve3( [
+        new THREE.Vector3( -10, 0, 10 ),
+        new THREE.Vector3( -5, 5, 5 ),
+        new THREE.Vector3( 0, 0, 0 ),
+        new THREE.Vector3( 5, -5, 5 ),
+        new THREE.Vector3( 10, 0, 10 )
+    ] );
+
+    const points = curve.getPoints( 50 );
+    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+    const material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
+
+    return (
+        <line
+            geometry={geometry}
+            material={material}
+        />
+        
+    );
+
+
+}
+
 export default function App2(props) {
 
     return (
-    <Canvas className="canvas" style={{backgroundColor:'#000000'}}>
+    <Canvas className="canvas" >
         <ambientLight />
-        <Loading />
+        {/* <Loading /> */}
+        <Curve />
         <OrbitControls />
     </Canvas>
     );
