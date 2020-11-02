ad1e64c3dc42445ba32cd49a43b30c39e54f9c4d
espisepi <sepinaco@gmail.com>
2020-11-02 02:17:42 +0100

    App3 all components included into scene

--- src/scenes/App3/state.js
+++ src/scenes/App3/state.js
@@ -1,16 +1,16 @@
 const state = {
     top: 0,
-    pages: 0,
+    pages: 0, //page is calculated in runtime by flexbox
     threshold: 4,
     mouse: [0, 0],
     content: [
       {
         tag: '00',
         text: `The Bacchic\nand Dionysiac\nRites`,
-        images: ['/images/BH41NVu.jpg', '/images/fBoIJLX.jpg', '/images/04zTfWB.jpg'],
+        images: ['assets/img/waternormals.jpg', 'assets/img/waternormals.jpg', 'assets/img/waternormals.jpg'],
       },
-      { tag: '01', text: `The Elysian\nMysteries`, images: ['/images/c4cA8UN.jpg', '/images/ajQ73ol.jpg', '/images/gZOmLNU.jpg'] },
-      { tag: '02', text: `The Hiramic\nLegend`, images: ['/images/mbFIW1b.jpg', '/images/mlDUVig.jpg', '/images/gwuZrgo.jpg'] },
+      { tag: '01', text: `The Elysian\nMysteries`, images: ['assets/img/waternormals.jpg', 'assets/img/waternormals.jpg', 'assets/img/waternormals.jpg'] },
+      { tag: '02', text: `The Hiramic\nLegend`, images: ['assets/img/waternormals.jpg', 'assets/img/waternormals.jpg', 'assets/img/waternormals.jpg'] },
     ],
     depthbox: [
       {
@@ -18,13 +18,13 @@ const state = {
         color: '#cccccc',
         textColor: '#ffffff',
         text: 'In a void,\nno one could say\nwhy a thing\nonce set in motion\nshould stop anywhere.',
-        image: '/images/cAKwexj.jpg',
+        image: 'assets/img/waternormals.jpg',
       },
       {
         depth: -5,
         textColor: '#272727',
         text: 'For why should it stop\nhere rather than here?\nSo that a thing\nwill either be at rest\nor must be moved\nad infinitum.',
-        image: '/images/04zTfWB.jpg',
+        image: 'assets/img/waternormals.jpg',
       },
     ],
     lines: [
