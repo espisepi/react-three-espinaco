8376eea9cbedd37e8f4ec1ba2d1e05a996f7029f
espisepi <sepinaco@gmail.com>
2020-11-27 20:25:46 +0100

    App12 ThirdPerson Animation Camera Controller (thanks to simondev (L) )

--- src/scenes/App12/App12.js
+++ src/scenes/App12/App12.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
+import React, {useEffect, useState} from 'react';
+import { Canvas, useFrame, useThree } from 'react-three-fiber';
 import { OrbitControls } from 'drei';
 import Loading from '../../components/Loading';
+import Stars from '../../drei-espinaco/Stars';
+
+import {BasicCharacterController, ThirdPersonCamera} from './simondevClasses';
 
 export function Scene() {
+
+    const {camera, scene} = useThree();
+
+    const [controls] = useState(()=>{
+        const params = {
+            camera: camera,
+            scene: scene,
+          }
+        return new BasicCharacterController(params);
+    });
+
+    const [thirdPersonCamera] = useState(()=>{
+        return new ThirdPersonCamera({
+            camera: camera,
+            target: controls,
+        });
+    });
+
+    useFrame((state, delta)=>{
+        if(controls) { controls.Update(delta) }
+        if(thirdPersonCamera){ thirdPersonCamera.Update(delta) }
+    });
+
     return(
         <>
         <ambientLight />
         <Loading />
-        <OrbitControls />
+        <Stars />
+        {/* <OrbitControls /> */}
         </>
     );
 }
