7f5df67e5c79dcc009bf6d8f819d0488887bf3ef
espisepi <sepinaco@gmail.com>
2020-11-29 23:09:57 +0100

    Joystick copied into drei-espinaco folder

--- src/scenes/App12/App12.js
+++ src/scenes/App12/App12.js
@@ -5,10 +5,7 @@ import Loading from '../../components/Loading';
 import Stars from '../../drei-espinaco/Stars';
 
 import SimondevPersonController from '../../drei-espinaco/simondev/SimondevPersonController';
-
-// https://loopmode.github.io/react-nipple/
-// https://codepen.io/YoannM/pen/gapmMG
-import ReactNipple from 'react-nipple';
+import Joystick from '../../drei-espinaco/Joystick';
 
 export function Scene() {
 
@@ -17,71 +14,19 @@ export function Scene() {
         <ambientLight />
         <Stars />
         <SimondevPersonController />
-
         </>
     );
 }
 
 export default function App12(props) {
 
-    const handleEvent = useCallback((evt, data)=>{
-
-        if (data?.direction?.y === 'up') {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'w', keyCode: '87' }));
-        } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'w', keyCode: '87' }));
-        }
-        if (data?.direction?.y === 'down') {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 's', keyCode: '83' }));
-        } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 's', keyCode: '83' }));
-        }
-        if (data?.direction?.x === 'left' && ( data?.angle?.degree >= 135.0 && data?.angle?.degree <= 225.0 ) ) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a', keyCode: '65' }));
-        } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'a', keyCode: '65' }));
-        }
-        if (data?.direction?.x === 'right'  && ( data?.angle?.degree <= 45.0 || data?.angle?.degree >= 315.0 ) ) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'd', keyCode: '68' }));
-        } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'd', keyCode: '68' }));
-        }
-        if (data?.force >= 1.0) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'shift', keyCode: '16' }));
-        } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'shift', keyCode: '16' }));
-        }
-
-    },[])
-
+    
     return (
     <>
     <Canvas className="canvas" style={{backgroundColor:'#000000', position: 'absolute'}}>
         <Scene />
     </Canvas>
-
-    <div>
-        <ReactNipple
-            options={{mode: 'dynamic', position: {top: '0', left: '0'}}}
-            style={{
-                // outline: `1px dashed red`,
-                width: '100%',
-                height: '100vh',
-                position: `absolute`,
-                zIndex: 10,
-            }}
-
-            onStart={handleEvent}
-            onEnd={handleEvent}
-            onMove={handleEvent}
-            onDir={handleEvent}
-            onPlain={handleEvent}
-            onShown={handleEvent}
-            onHidden={handleEvent}
-            onPressure={handleEvent}
-        />
-    </div>
-    
+    <Joystick />
     </>
     );
 }
