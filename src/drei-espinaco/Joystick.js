2e167eba7ec2802d342f7e01adc5a4599e177140
espisepi <sepinaco@gmail.com>
2020-12-06 20:51:56 +0100

    Joystick with code attribute filled

--- src/drei-espinaco/Joystick.js
+++ src/drei-espinaco/Joystick.js
@@ -7,32 +7,32 @@ import ReactNipple from 'react-nipple';
 
 export default function Joystick() {
 
-    const handleEvent = useCallback((evt, data)=>{
+    const handleEvent = useCallback((evt, data)=>{ 
 
         if (data?.direction?.y === 'up' && ( data?.angle?.degree >= 15.0 && data?.angle?.degree <= 165.0 ) ) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'w', keyCode: '87' }));
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'w', keyCode: '87', code: 'KeyW' }));
         } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'w', keyCode: '87' }));
+            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'w', keyCode: '87', code: 'KeyW' }));
         }
         if (data?.direction?.y === 'down' && ( data?.angle?.degree >= 195.0 && data?.angle?.degree <= 345.0 ) ) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 's', keyCode: '83' }));
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: 's', keyCode: '83', code: 'KeyS' }));
         } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 's', keyCode: '83' }));
+            document.dispatchEvent(new KeyboardEvent('keyup', { key: 's', keyCode: '83', code: 'KeyS' }));
         }
         if (data?.direction?.x === 'left' && ( data?.angle?.degree >= 135.0 && data?.angle?.degree <= 225.0 ) ) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a', keyCode: '65' }));
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a', keyCode: '65', code: 'KeyA' }));
         } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'a', keyCode: '65' }));
+            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'a', keyCode: '65', code: 'KeyA' }));
         }
         if (data?.direction?.x === 'right'  && ( data?.angle?.degree <= 45.0 || data?.angle?.degree >= 315.0 ) ) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'd', keyCode: '68' }));
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'd', keyCode: '68', code: 'KeyD' }));
         } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'd', keyCode: '68' }));
+            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'd', keyCode: '68', code: 'KeyD' }));
         }
         if (data?.force >= 1.0) {
-            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'shift', keyCode: '16' }));
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: 'shift', keyCode: '16', code: 'ShiftLeft' }));
         } else {
-            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'shift', keyCode: '16' }));
+            document.dispatchEvent(new KeyboardEvent('keyup', { key: 'shift', keyCode: '16', code: 'ShiftLeft' }));
         }
 
     },[]);
