303f629f5a6fbe913b7558be644345306076a244
espisepi <sepinaco@gmail.com>
2020-12-07 23:58:58 +0100

    clean code in home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,20 +8,10 @@ import Stars from '../drei-espinaco/Stars';
 export default function Home() {
     return (
         <>
-        <div style={{display:'flex', flexFlow:'row nowrap', alignItems:'center', justifyContent:'center', position:'relative', top:'50vh', zIndex:'10' }}>
-            
-                <Link to="/app6" className="card2 gatacattana" >
-                        
-                        
-                </Link>
-            
-                <Link to="/app14" className="card2 elane" >
-                    
-                </Link>
-                <Link to="/app1" className="card2 shake070" >
-                        
-                       
-                </Link>
+        <div style={{display:'flex', flexFlow:'row nowrap', alignItems:'center', justifyContent:'center', position:'relative', top:'50vh', zIndex:'10' }}>        
+                <Link to="/app6" className="card2 gatacattana"></Link>
+                <Link to="/app14" className="card2 elane"></Link>
+                <Link to="/app1" className="card2 shake070"></Link>
         </div>
         <Canvas style={{position: 'absolute', top:'0', margin:'0'}}>
             <Stars />
