c119972f4be7bd74d3a1d9e5c36f7a80b2204a9f
espisepi <sepinaco@gmail.com>
2020-12-07 23:50:32 +0100

    home page with css

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,18 +2,30 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './Home.css';
 
+import { Canvas } from 'react-three-fiber';
+import Stars from '../drei-espinaco/Stars';
+
 export default function Home() {
     return (
-        <div style={{display:'flex', flexFlow:'row nowrap'}}>
-            <div style={{width:'200px', height:'200px'}}>
-                <Link to="/app6">Gatacattana</Link>
-            </div>
-            <div style={{width:'200px', height:'200px'}}>
-                <Link to="/app14">Elane</Link>
-            </div>
-            <div style={{width:'200px', height:'200px'}}>
-                <Link to="/app1">070Shake</Link>
-            </div>
+        <>
+        <div style={{display:'flex', flexFlow:'row nowrap', alignItems:'center', justifyContent:'center', position:'relative', top:'50vh', zIndex:'10' }}>
+            
+                <Link to="/app6" className="card2 gatacattana" >
+                        
+                        
+                </Link>
+            
+                <Link to="/app14" className="card2 elane" >
+                    
+                </Link>
+                <Link to="/app1" className="card2 shake070" >
+                        
+                       
+                </Link>
         </div>
+        <Canvas style={{position: 'absolute', top:'0', margin:'0'}}>
+            <Stars />
+        </Canvas>
+        </>
     );
 }
