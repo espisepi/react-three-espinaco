ecd76684a6a60e88bd752fdb0f47518e5375e5cc
espisepi <sepinaco@gmail.com>
2020-12-17 18:58:46 +0100

    InstancedMesh now can modify their objects

--- src/drei-espinaco/InstancedMesh.js
+++ src/drei-espinaco/InstancedMesh.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState, useEffect } from 'react';
+import React, { useMemo, useRef, useState, useEffect, useCallback } from 'react';
 import * as THREE from 'three';
 import { useLoader, useFrame } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
@@ -10,10 +10,11 @@ const InstancedMesh = ({
     objects
 
 }) => {
-    const tempObject = new THREE.Object3D()
+
     const ref = useRef()
 
     useEffect(()=>{
+      const tempObject = new THREE.Object3D();
       objects.forEach((object, id) => {
         const position = object.position ? object.position : [0,0,0];
         const rotation = object.rotation ? object.rotation : [0,0,0];
@@ -23,9 +24,63 @@ const InstancedMesh = ({
         tempObject.scale.set(...scale);
         tempObject.updateMatrix();
         ref.current.setMatrixAt(id, tempObject.matrix);
+
+        // To get objects with all attributes in memory
+        object.position = position;
+        object.rotation = rotation;
+        object.scale = scale;
       });
       ref.current.instanceMatrix.needsUpdate = true;
     },[ref, objects]);
+
+
+    const tempObject = useMemo(()=> new THREE.Object3D(),[]);
+    const modifyImesh = useCallback((imesh, id, object)=>{
+      if(objects[id]){
+        const position = object.position ? object.position : objects[id].position;
+        const rotation = object.rotation ? object.rotation : objects[id].rotation;
+        const scale = object.scale ? object.scale : objects[id].scale;
+        tempObject.position.set(...position); 
+        tempObject.rotation.set(...rotation);
+        tempObject.scale.set(...scale);
+        tempObject.updateMatrix();
+        imesh.setMatrixAt(id, tempObject.matrix);
+        imesh.instanceMatrix.needsUpdate = true;
+      } else {
+        console.log('InstancedMesh: modifyImesh method is called with an id out of array objects')
+      }
+    });
+
+    const setImeshFromObjectsMod = useCallback((imesh, objectsMod)=>{
+      for(let indexObjectsMod = 0; indexObjectsMod < objectsMod.length; indexObjectsMod++ ){
+        const objectMod = objectsMod[indexObjectsMod];
+        for(let indexIds = 0; indexIds < objectMod.ids.length; indexIds++) {
+          const id = objectMod.ids[indexIds];
+          modifyImesh(imesh,id,objectMod.object);
+        }
+      }
+    })
+
+    const createObjectsMod = useCallback((state)=>{
+      const objectsMod = [
+        {
+          ids: [0,1],
+          object: {
+            rotation: [0,state.clock.getElapsedTime(),0]
+          }
+        }
+      ];
+      return objectsMod;
+  });
+
+    const modifyObjects = useCallback((imesh,state)=>{
+      const objectsMod = createObjectsMod(state);
+      setImeshFromObjectsMod(imesh,objectsMod);
+    },[])
+
+    useFrame((state)=>{
+      modifyObjects(ref.current, state);
+    });
   
     return (
       <instancedMesh ref={ref} args={[geometry, material, objects.length]} >
