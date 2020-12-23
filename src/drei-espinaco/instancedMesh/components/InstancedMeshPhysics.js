9ed23915ee43f3b2f8ba804d7dc8f9d7a934a37f
espisepi <sepinaco@gmail.com>
2020-12-23 01:30:48 +0100

    App24 Water WallDestroy

--- src/drei-espinaco/instancedMesh/components/InstancedMeshPhysics.js
+++ src/drei-espinaco/instancedMesh/components/InstancedMeshPhysics.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import {useBox} from 'use-cannon';
 import InstancedMesh from './InstancedMesh';
 
-export function CreatePhysicBox({props, visible}) {
+export function CreatePhysicBox({props, visible = true}) {
     const [ref] = useBox(() => ({...props}));
     return (
     <mesh ref={ref}>
@@ -13,7 +13,7 @@ export function CreatePhysicBox({props, visible}) {
     );
 }
 
-export function CreatePhysicBoxes({objects, visible = true}) {
+export function CreatePhysicBoxes({objects, visible}) {
     const physicMeshes = useMemo(()=>{
         const physicMeshes = [];
         objects.forEach((object) => {
@@ -41,7 +41,7 @@ export function CreatePhysicBoxes({objects, visible = true}) {
 }
 
 export default function InstancedMeshPhysics({geometry=new THREE.BoxBufferGeometry(1,1,1), material=new THREE.MeshBasicMaterial({color:'red'}), objects=[], createObjectsModBoolean = false, visible = false}){
-
+    
     // if objects array is empty, initialize it with these default values
     if(objects.length === 0) {
         for(let i = 0; i< 5; i++){
