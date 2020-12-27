89174dcb3b2b09e8058e43f0ad8b1379e7b7aad3
espisepi <sepinaco@gmail.com>
2020-12-27 15:20:42 +0100

    App30 mineralGallery, model in scene

--- src/scenes/App30/App30.js
+++ src/scenes/App30/App30.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Suspense, useState } from 'react';
 import { Canvas } from 'react-three-fiber';
 import { OrbitControls } from 'drei';
 import Loading from '../../components/Loading';
 
+import Stars from '../../drei-espinaco/Stars';
+
+import {InstancedMesh, InstancedMeshPhysics, InstancedMeshes, InstancedFBX, InstancedGLTF, InstancedGLTFPhysics, InstancedPhysics} from '../../drei-espinaco/instancedMesh/';
+
+function Model3D({src}) {
+    const objects = [{position:[0,0,0],scale:[0.5,0.5,0.5]}];
+    return <InstancedGLTF src={src} objects={objects} />;
+}
+
 export function Scene() {
+    const [modelSrc, setModelSrc] = useState('assets/obj/gallery_chapel_baked/scene.gltf');
     return(
         <>
         <ambientLight />
-        <Loading />
+        <Stars />
+        <Suspense fallback={<Loading />}>
+            <Model3D src={modelSrc} />
+        </Suspense>
         <OrbitControls />
         </>
     );
