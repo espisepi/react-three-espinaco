3910ac8099e07cb7087816c7e4ecf56d6938f78c
espisepi <sepinaco@gmail.com>
2021-01-08 18:05:04 +0100

    onclick sphere change 360 image working in app34 360 Gallery

--- src/scenes/App34/App34.js
+++ src/scenes/App34/App34.js
@@ -1,6 +1,6 @@
 // https://codesandbox.io/s/r3f-sky-dome-forked-rj0tn?file=/src/index.js:633-1162 (L)
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useCallback} from 'react';
 import * as THREE from 'three';
 import { Canvas, useLoader } from 'react-three-fiber';
 import { OrbitControls } from 'drei';
@@ -9,47 +9,53 @@ import Loading from '../../components/Loading';
 import { proxy, useProxy } from "valtio";
 import { Suspense } from 'react';
 
-var hotspot0 = {
-    location: location0,
+const hotspot0 = {
+    position: [0,2,0],
+    img: ''
+}
+const hotspot1 = {
     position: [0,2,0],
     img: ''
 }
 
-var location0 = {
+const location0 = {
     env:'assets/env/360jpg/cannon.jpg',
-    children: [hotspot0]
+    children: [hotspot1]
 }
-
-var location1 = {
+const location1 = {
     env:'assets/env/360jpg/lilienstein.jpg',
     children: [hotspot0]
 }
+hotspot0.location = location0;
+hotspot1.location = location1;
 
 
-
-var hotspotsState = {
-        current: location1,
-};
+const hotspotsState = proxy({
+        current: location0,
+});
 
 export function Scene() {
-    const envSrc = hotspotsState.current.env;
+    const snapHotspots = useProxy(hotspotsState);
+    const envSrc = snapHotspots.current.env;
     const texture = useLoader(THREE.TextureLoader, envSrc);
     useEffect(()=>{
     
-    },[])
-    
+    },[]);
+
+    const handleOnClick = useCallback(({location})=>{
+        hotspotsState.current = location;
+    },[]);
 
     return(
         <>
         <ambientLight />
-        <Loading />
         <OrbitControls />
         <group>
         <mesh>
             <sphereBufferGeometry args={[500, 60, 40]} />
             <meshBasicMaterial map={texture} side={THREE.BackSide} />
         </mesh>
-        <mesh position={hotspotsState.current.children[0].position}>
+        <mesh position={snapHotspots.current.children[0].position} onClick={()=>handleOnClick(snapHotspots.current.children[0])}>
             <sphereGeometry args={[1.25, 32, 32]} />
             <meshBasicMaterial color="white" />
         </mesh>
