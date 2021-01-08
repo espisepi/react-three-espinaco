eb4be761ab8827c41de6ee525d0bb3bcbd27e52d
espisepi <sepinaco@gmail.com>
2021-01-08 23:01:38 +0100

    Sphere coincide con background al cambiar el environment

--- src/scenes/App34/App34.js
+++ src/scenes/App34/App34.js
@@ -44,6 +44,7 @@ const texturesState = proxy({
 
 export function Scene() {
     const snapHotspots = useProxy(hotspotsState);
+    const snapAnimation = useProxy(animationState);
     const envSrc = snapHotspots.current.env;
     const texture = useLoader(THREE.TextureLoader, envSrc);
     texture.mapping = THREE.EquirectangularReflectionMapping;
@@ -51,7 +52,7 @@ export function Scene() {
     texturesState.new = texture;
 
     // equirectangular background
-    const {scene} = useThree();
+    const {scene, camera} = useThree();
     useEffect(()=>{
         scene.background = texture;
     },[texture]);
@@ -61,11 +62,20 @@ export function Scene() {
         hotspotsState.current = location;
         animationState.run = true;
     },[]);
+
+    // Sphere coincide con background
+    const meshLoading = useRef();
+    useEffect(()=>{
+        if(meshLoading.current){
+            camera.add(meshLoading.current)
+        }
+    },[meshLoading.current]);
+
     return(
         <>
         <ambientLight />
         <group>
-        <mesh>
+        <mesh ref={meshLoading} visible={snapAnimation.run}>
             <sphereBufferGeometry args={[500, 60, 40]} />
             <meshBasicMaterial map={texture} side={THREE.DoubleSide} />
         </mesh>
@@ -90,14 +100,28 @@ function RunAnimation() {
     const animationSnap = useProxy(animationState);
     const texturesSnap = useProxy(texturesState);
     const [iTime, setITime] = useState(0);
-    console.log(texturesSnap)
+    
 
     const [texture, setTexture] = useState(texturesSnap.old);
+
+    // const mesh = useRef();
     useFrame(({camera})=>{
         if(animationSnap.run){
             setITime( iTime => iTime += 0.06 );
             camera.zoom = 1.0 + ( ( (1.0 + Math.cos(3.14 +iTime) ) / 2.0 ) * 10.0 );
             camera.updateProjectionMatrix();
+            if(iTime <= 3.14){
+                // console.log(texturesSnap.old.clone())
+                // console.log(mesh.current.material)
+                const textureTemp = texturesSnap.old.clone();
+                // mesh.current.material.envMap = textureTemp.clone();
+                // textureTemp.mapping = THREE.EquirectangularReflectionMapping;
+                // textureTemp.encoding = THREE.sRGBEncoding;
+                // setTexture(textureTemp);
+            }else{
+                // setTexture(texturesSnap.new);
+            }
+
             if(iTime >= 3.14 * 2.0) {
                 setITime( 0 );
                 animationState.run = false;
@@ -107,11 +131,9 @@ function RunAnimation() {
         }
 
     });
-    return (null
-        // <mesh>
-        //     <sphereBufferGeometry args={[500, 60, 40]} />
-        //     <meshBasicMaterial map={texture} side={THREE.DoubleSide} />
-        // </mesh>
+    
+    return (
+        null
     );
 }
 
