7a111c21ba41e6617c98a63f2d95ab9b56e0e70b
espisepi <sepinaco@gmail.com>
2021-01-13 23:18:43 +0100

    PicturesDisplay in separate file App35 Gallery jipis

--- src/scenes/App35/App35.js
+++ src/scenes/App35/App35.js
@@ -12,80 +12,9 @@ import GroundPhysic from '../../the-gallery/components/Ground/GroundPhysic';
 import {InstancedMesh, InstancedMeshPhysics, InstancedMeshes, InstancedFBX, InstancedGLTF, InstancedGLTFPhysics, InstancedPhysics} from '../../drei-espinaco/instancedMesh/';
 import { createMapsPoints, createMapPoints, transformPointsToObjects } from '../../drei-espinaco/points-creator/';
 
-function Picture({
-    img='assets/3D/Portrait/textures/initialShadingGroup_baseColor.jpg',
-    position=[0,0,0],
-    rotation=[0,0,0],
-    scale=[1,1,1]
-}) {
-    const map = useLoader(THREE.TextureLoader,img);
-  
-    return (
-        <mesh scale={scale} position={position} rotation={rotation}>
-            <planeBufferGeometry attach="geometry" args={[1,1,1,1]} />
-            <meshLambertMaterial attach="material" map={map} side={THREE.BackSide} />
-        </mesh>
-    )
-}
+import PicturesDisplay from './PicturesDisplay';
 
-function PicturesDisplay() {
-    const pictures = useMemo(()=>{
-        return [
-            {
-                img: 'assets/img/jipis/charls/doggy.jpeg',
-                position: [19.4, 5, 0],
-                rotation: [0,Math.PI/2,0],
-                scale: [10,10,1],
-                display: {
-                    position: [20, 5, 0],
-                    rotation: [0,Math.PI/2,0],
-                    scale: [15, 18, 1]
-                }
-            },
-            {
-                img: 'assets/img/jipis/charls/doggy2.jpeg',
-                position: [19.4, 5, 25],
-                rotation: [0,Math.PI/2,0],
-                scale:[10,10,1],
-                display: {
-                    position: [20, 5, 25],
-                    rotation: [0,Math.PI/2,0],
-                    scale: [15, 18, 1]
-                }
-            }
-        ];
-    });
 
-    const displays = pictures.map(p => p.display);
-    const displaysPhysics = displays.map(d => ({propsPhysics: {
-        position: d.position,
-        rotation: d.rotation,
-        args: d.scale
-    }}));
-    console.log(displaysPhysics);
-
-    const alphaMap = useMemo(() => new THREE.TextureLoader().load("assets/Textures/BiancoMarble/BIANCO-ao.jpg"), []);
-    const diffuseMap = useMemo(() => new THREE.TextureLoader().load("assets/Textures/BiancoMarble/BIANCO-diffuse.jpg"), []);
-    const normalMap = useMemo(() => new THREE.TextureLoader().load("assets/Textures/BiancoMarble/BIANCO-normal.jpg"), []);
-
-    return (
-        <>
-        {pictures.map((p,i) => (<Picture key={i} img={p.img} position={p.position} rotation={p.rotation} scale={p.scale} />))}
-        <InstancedMesh 
-            geometry={new THREE.BoxBufferGeometry(1,1,1)}
-            material={new THREE.MeshStandardMaterial({
-                side: THREE.DoubleSide,
-                alphaMap: alphaMap,
-                diffuseMap: diffuseMap,
-                normalMap: normalMap,
-                roughness: 0.5
-            })}
-            objects={displays}
-        />
-        <InstancedPhysics objects={displaysPhysics} visible={false} />
-        </>
-    );
-}
 
 export function Scene() {
     
---
+++ src/scenes/App35/PicturesDisplay.js
@@ -0,0 +1,80 @@
+import React, { useMemo } from 'react';
+import * as THREE from 'three';
+import { useLoader } from 'react-three-fiber';
+import {InstancedMesh, InstancedPhysics} from '../../drei-espinaco/instancedMesh/';
+
+
+function Picture({
+    img='assets/3D/Portrait/textures/initialShadingGroup_baseColor.jpg',
+    position=[0,0,0],
+    rotation=[0,0,0],
+    scale=[1,1,1]
+}) {
+    const map = useLoader(THREE.TextureLoader,img);
+  
+    return (
+        <mesh scale={scale} position={position} rotation={rotation}>
+            <planeBufferGeometry attach="geometry" args={[1,1,1,1]} />
+            <meshLambertMaterial attach="material" map={map} side={THREE.BackSide} />
+        </mesh>
+    )
+}
+
+export default function PicturesDisplay() {
+    const pictures = useMemo(()=>{
+        return [
+            {
+                img: 'assets/img/jipis/charls/doggy.jpeg',
+                position: [19.4, 5, 0],
+                rotation: [0,Math.PI/2,0],
+                scale: [10,10,1],
+                display: {
+                    position: [20, 5, 0],
+                    rotation: [0,Math.PI/2,0],
+                    scale: [15, 18, 1]
+                }
+            },
+            {
+                img: 'assets/img/jipis/charls/doggy2.jpeg',
+                position: [19.4, 5, 25],
+                rotation: [0,Math.PI/2,0],
+                scale:[10,10,1],
+                display: {
+                    position: [20, 5, 25],
+                    rotation: [0,Math.PI/2,0],
+                    scale: [15, 18, 1]
+                }
+            }
+        ];
+    });
+
+    const displays = pictures.map(p => p.display);
+    const displaysPhysics = displays.map(d => ({propsPhysics: {
+        position: d.position,
+        rotation: d.rotation,
+        args: d.scale
+    }}));
+    console.log(displaysPhysics);
+
+    const alphaMap = useMemo(() => new THREE.TextureLoader().load("assets/Textures/BiancoMarble/BIANCO-ao.jpg"), []);
+    const diffuseMap = useMemo(() => new THREE.TextureLoader().load("assets/Textures/BiancoMarble/BIANCO-diffuse.jpg"), []);
+    const normalMap = useMemo(() => new THREE.TextureLoader().load("assets/Textures/BiancoMarble/BIANCO-normal.jpg"), []);
+
+    return (
+        <>
+        {pictures.map((p,i) => (<Picture key={i} img={p.img} position={p.position} rotation={p.rotation} scale={p.scale} />))}
+        <InstancedMesh 
+            geometry={new THREE.BoxBufferGeometry(1,1,1)}
+            material={new THREE.MeshStandardMaterial({
+                side: THREE.DoubleSide,
+                alphaMap: alphaMap,
+                diffuseMap: diffuseMap,
+                normalMap: normalMap,
+                roughness: 0.5
+            })}
+            objects={displays}
+        />
+        <InstancedPhysics objects={displaysPhysics} visible={false} />
+        </>
+    );
+}
