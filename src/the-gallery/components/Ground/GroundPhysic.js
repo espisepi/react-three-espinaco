32d575f83394609eb1bb6fe9b714a8a397cbfe9a
espisepi <sepinaco@gmail.com>
2021-01-17 21:14:04 +0100

    GroundPhysics with position parameter

--- src/the-gallery/components/Ground/GroundPhysic.js
+++ src/the-gallery/components/Ground/GroundPhysic.js
@@ -3,11 +3,11 @@ import { usePlane } from "use-cannon";
 import * as THREE from 'three';
 import { Reflector } from '@react-three/drei';
 
-const GroundPhysic = ({visible=false}) => {
+const GroundPhysic = ({visible=false, position=[0,0.1,22]}) => {
 
     const [ref] = usePlane(() => ({ 
         rotation: [-Math.PI / 2, 0, 0],
-        position: [0, 0.1, 22],
+        position: position,
     }));
 
     return (
