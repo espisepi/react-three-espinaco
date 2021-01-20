c52fb3960108c0f50912d8d79e13a3de47476f0d
espisepi <sepinaco@gmail.com>
2021-01-20 01:13:38 +0100

    fix Loading App35, loading mesh appear with respect camera position

--- src/scenes/App35/Loading.js
+++ src/scenes/App35/Loading.js
@@ -1,13 +1,14 @@
-import React, { useRef } from 'react';
-import { useFrame } from 'react-three-fiber';
+import React, { useRef, useState } from 'react';
+import { useFrame, useThree } from 'react-three-fiber';
 import { Text, Html } from 'drei';
 
 export default function Loading() {
     
+    const { camera } = useThree();
+    const [frozenPosition] = useState(camera.position);
     const mesh = useRef(null);
-
     useFrame(({camera, scene}) => {
-        camera.position.set(0,0,-5);
+        camera.position.set(frozenPosition.x, frozenPosition.y, frozenPosition.z);
         if( mesh ) {
             mesh.current.rotation.y += 0.05;
         }
@@ -16,7 +17,7 @@ export default function Loading() {
     return (
         <>
         <ambientLight />
-        <group position={[0,0,-10]}>
+        <group position={[frozenPosition.x,frozenPosition.y,frozenPosition.z - 10]}>
             <mesh ref={mesh}>
                 <boxBufferGeometry attach='geometry' args={[1,1,1]} />
                 <meshStandardMaterial attach='material' color='red' wireframe={true} />
