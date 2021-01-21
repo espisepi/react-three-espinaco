d53a285a17ad13d7c0c665e7a4164796c3de1087
espisepi <sepinaco@gmail.com>
2021-01-21 20:14:01 +0100

    DecalGeometry working in App36 Tattoo model

--- src/scenes/App36/App36.js
+++ src/scenes/App36/App36.js
@@ -1,27 +1,49 @@
 import React, {Suspense, useEffect} from 'react';
-import { Canvas, useThree } from 'react-three-fiber';
+import { Canvas, useLoader, useThree } from 'react-three-fiber';
 import * as THREE from 'three';
 import { OrbitControls, useGLTF } from 'drei';
 import Loading from '../../components/Loading';
 import { DecalGeometry } from 'three/examples/jsm/geometries/DecalGeometry.js';
 
-
+// https://threejs.live/#/webgl_decals
 function Model(){
 
     const { scene } = useThree();
-    // const gltf = useGLTF('assets/obj/shoe-draco.glb');
+    const gltf = useGLTF('assets/obj/LeePerrySmith/LeePerrySmith.glb');
+    const [map,specularMap,normalMap] = useLoader(THREE.TextureLoader, ['assets/obj/LeePerrySmith/Map-COL.jpg','assets/obj/LeePerrySmith/Map-COL.jpg','assets/obj/LeePerrySmith/Infinite-Level_02_Tangent_SmoothUV.jpg'])
+
+    const decalMap = useLoader(THREE.TextureLoader, 'assets/img/highkili.png');
+    
 
     // TODO: Hacer el dispose de los elementos
     useEffect(()=>{
-        const mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2,2,10,10), new THREE.MeshBasicMaterial({color:'blue'}));
+        const mesh = gltf.scene.children[0];
+        mesh.material = new THREE.MeshPhongMaterial( {
+            specular: 0x111111,
+            map: map,
+            specularMap: specularMap,
+            normalMap: normalMap,
+            shininess: 25
+        } );
         scene.add(mesh);
 
-        console.log(mesh)
-        const material = new THREE.MeshBasicMaterial({color:'red'})
-        const position = new THREE.Vector3(0,0,0);
+        const decalMaterial = new THREE.MeshPhongMaterial({
+            specular: 0x444444,
+            map: decalMap,
+            // normalMap: decalNormal,
+            normalScale: new THREE.Vector2( 1, 1 ),
+            shininess: 30,
+            transparent: true,
+            depthTest: true,
+            depthWrite: false,
+            polygonOffset: true,
+            polygonOffsetFactor: - 4,
+            wireframe: false
+        });
+        const position = new THREE.Vector3(0,0,-1);
         const rotation = new THREE.Euler(0,0,0);
-        const scale = new THREE.Vector3(1,1,1);
-        const decal = new THREE.Mesh( new DecalGeometry( mesh, position, rotation, scale ), material );
+        const scale = new THREE.Vector3(5,5,5);
+        const decal = new THREE.Mesh( new DecalGeometry( mesh, position, rotation, scale ), decalMaterial );
         scene.add(decal);
     },[]);
 
@@ -36,7 +58,9 @@ function Model(){
 export function Scene() {
     return(
         <>
-        <ambientLight />
+        <ambientLight args={0x443333} />
+        <directionalLight args={[0xffddcc, 1]} position={[1, 0.75, 0.5]} />
+        <directionalLight args={[0xccccff, 1]} position={[-1, 0.75, 0.5]} />
         <Suspense fallback={<Loading />} >
             <Model />
         </Suspense>
