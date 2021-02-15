5a5213b50cc230061f34c4439b76e0d7a02baaad
espisepi <sepinaco@gmail.com>
2021-02-15 22:44:02 +0100

    pictures in scene App40

--- src/scenes/App40/App40.js
+++ src/scenes/App40/App40.js
@@ -1,7 +1,7 @@
 import React, { Suspense, useMemo, useCallback, useState, useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { Canvas, useFrame, useLoader, useThree } from 'react-three-fiber';
-import { Stats, OrbitControls, TransformControls } from 'drei';
+import { Canvas, useFrame, useLoader, useThree, extend } from 'react-three-fiber';
+import { Stats, OrbitControls, TransformControls, Plane, useTexture } from 'drei';
 import Loading from '../App38/Loading';
 
 import Joystick from '../../drei-espinaco/Joystick';
@@ -17,14 +17,86 @@ import Fullscreen from '../../drei-espinaco/Fullscreen';
 import MeshTransformControls from '../../drei-espinaco/MeshTransformControls';
 
 import { a as aDom } from '@react-spring/web';
+import { a } from '@react-spring/three';
 import useYScroll from './useYScroll';
 
+import { shaderMaterial } from 'drei';
+
+const MyMaterial = shaderMaterial(
+    { map: new THREE.Texture(), repeats: 1 },
+    `
+  varying vec2 vUv;
+  
+  void main()	{
+    vUv = uv;
+    
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+  }
+  `,
+    `
+  varying vec2 vUv;
+  uniform float repeats;
+  uniform sampler2D map;
+  
+  float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+  }
+  
+  void main(){
+    vec2 uv = vUv;
+  
+    uv *= repeats;
+    uv = fract(uv);
+  
+    vec3 color = vec3(
+      texture2D(map, uv).r,
+      texture2D(map, uv + vec2(0.01,0.01)).g,
+      texture2D(map, uv - vec2(0.01,0.01)).b
+    );
+    
+    gl_FragColor = vec4(color,1.0);
+  }
+  `
+);
+  
+extend({ MyMaterial });
+
+export function Picture({src, ...props}){
+    const map = useTexture(src);
+
+    return(
+        <Plane {...props} >
+            <meshBasicMaterial map={map} />
+        </Plane>
+    );
+}
+
+const pictures = [
+    {
+        src:'https://source.unsplash.com/random/400x400'
+    },
+    {
+        src:'https://source.unsplash.com/random/400x400'
+    },
+    {
+        src:'https://source.unsplash.com/random/400x400'
+    },
+    {
+        src:'https://source.unsplash.com/random/400x400'
+    },
+]
 
 export function ScenePrincipal({y}) {
-    console.log(y);
+    
     return(
         <>
-        <Loading />
+        <a.group position-y={y.to((y) => ( y / 500 ) * 25 )} >
+            {pictures.map( (object, i) => (
+                <Picture key={`picture${i}`} src={object.src} position={[0,i*-7,0]} scale={[5,5,5]} rotation={[-0.5,0,0]} />
+            ))}
+        </a.group>
         </>
     );
 }
@@ -46,7 +118,9 @@ export function RunApp40(props) {
     <>
     <Canvas className="canvas" style={{backgroundColor:'#000000', position:'absolute'}}>
         {/* <Stats /> */}
-        <ScenePrincipal y={y} />
+        <Suspense fallback={<Loading />}>
+            <ScenePrincipal y={y} />
+        </Suspense>
         {/* <OrbitControls /> */}
     </Canvas>
     <Fullscreen />
