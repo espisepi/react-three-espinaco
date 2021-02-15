7000d2d6da04b70269f3fc202ee0be3ba29a4437
espisepi <sepinaco@gmail.com>
2021-02-15 21:29:26 +0100

    scrolling bar in App40

--- src/scenes/App40/App40.js
+++ src/scenes/App40/App40.js
@@ -8,22 +8,20 @@ import Joystick from '../../drei-espinaco/Joystick';
 import { Physics, useBox } from 'use-cannon';
 import Player from '../../the-gallery/components/Player/Player';
 import GroundPhysic from '../../the-gallery/components/Ground/GroundPhysic';
-
 import {InstancedMesh, InstancedMeshPhysics, InstancedMeshes, InstancedFBX, InstancedGLTF, InstancedGLTFPhysics, InstancedPhysics} from '../../drei-espinaco/instancedMesh/';
 import { createMapsPoints, createMapPoints, transformPointsToObjects } from '../../drei-espinaco/points-creator/';
-
 import Scene01 from '../App38/scenes/Scene01';
 import Scene02 from '../App38/scenes/Scene02';
-
 import { proxy, useProxy } from "valtio";
-
 import Fullscreen from '../../drei-espinaco/Fullscreen';
-
 import MeshTransformControls from '../../drei-espinaco/MeshTransformControls';
 
+import { a as aDom } from '@react-spring/web';
+import useYScroll from './useYScroll';
 
 
-export function ScenePrincipal() {
+export function ScenePrincipal({y}) {
+    console.log(y);
     return(
         <>
         <Loading />
@@ -31,16 +29,28 @@ export function ScenePrincipal() {
     );
 }
 
+export const bar = {
+    position:'absolute',
+    top:0,
+    left:0,
+    width:'4px',
+    background: 'lightcoral',
+    zIndex:10
+}
+
 export function RunApp40(props) {
 
+    const [y] = useYScroll([-100, 2400], { domTarget: window });
+    
     return (
     <>
     <Canvas className="canvas" style={{backgroundColor:'#000000', position:'absolute'}}>
         {/* <Stats /> */}
-        <ScenePrincipal />
-        <OrbitControls />
+        <ScenePrincipal y={y} />
+        {/* <OrbitControls /> */}
     </Canvas>
     <Fullscreen />
+    <aDom.div style={{...bar, height: y.interpolate([-100, 2400], ['0%', '100%']) }} />
     </>
     );
 }
