c574a7ed75b26042afd967cd6e5ab298d0d53e12
espisepi <sepinaco@gmail.com>
2021-02-27 03:46:35 +0100

    App42 model added

--- src/scenes/App42/App42.js
+++ src/scenes/App42/App42.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
-import { OrbitControls } from 'drei';
+import { OrbitControls, useGLTF } from 'drei';
 import Loading from '../../components/Loading';
+
+export function Catedral(props) {
+    const {nodes} = useGLTF('assets/obj/vr_gallery_round/scene.gltf');
+    console.log(nodes.root)
+    return (
+    <group {...props} >
+        <primitive object={nodes.root} rotation={[-Math.PI / 2, 0 ,0 ]}/>
+    </group>
+    );
+}
 
 export function Scene() {
     return(
         <>
-        <ambientLight />
-        <Loading />
+        {/* <ambientLight /> */}
+        <pointLight position={[0,2,0]} />
+        <Suspense fallback={<Loading/>}>
+            <Catedral />
+        </Suspense>
         <OrbitControls />
         </>
     );
