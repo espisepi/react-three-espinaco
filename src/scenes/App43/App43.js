ca24c434cbc9c638885606c07e430341bdc83b35
espisepi <sepinaco@gmail.com>
2021-03-01 01:37:04 +0100

    GalleryUI App43 - arrows camera movement working

--- src/scenes/App43/App43.js
+++ src/scenes/App43/App43.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useCallback, useState } from 'react';
+import React, { Suspense, useCallback, useState, useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import { Canvas, useLoader, useThree, useFrame } from 'react-three-fiber';
 import { OrbitControls } from 'drei';
@@ -12,7 +12,8 @@ const [useStore, api] = create((set, get) => {
         camera: undefined,
         cameraPositions: [
             [0,0,0],
-            [0,0,6]
+            [0,0,6],
+            [2,0,0]
         ],
         topNormalized: 0, // Range [0,1]
         setTopNormalized: (topNormalized) => {
@@ -22,6 +23,10 @@ const [useStore, api] = create((set, get) => {
             {
                 img: 'assets/img/gallery/lion.jpg',
                 position: [0,0,-1]
+            },
+            {
+                img: 'assets/img/gallery/tiger.jpg',
+                position: [2,0,-1]
             }
         ],
         actions: {
@@ -35,7 +40,7 @@ const [useStore, api] = create((set, get) => {
 export function Picture({img, ...props}) {
     const texture = useLoader(THREE.TextureLoader, img);
     return (
-        <mesh {...props} >
+        <mesh {...props} visible={true}>
             <planeBufferGeometry attach='geometry' args={[1,1]} />
             <meshBasicMaterial attach='material' map={texture} />
         </mesh>
@@ -44,32 +49,41 @@ export function Picture({img, ...props}) {
 
 export function Arrows({}) {
     const cameraPositions = useStore( state => state.cameraPositions );
-    const [topNormalized, setTopNormalized] = useStore( state => [state.topNormalized,state.setTopNormalized]);
-    const [nextPoint, setNextPoint] = useState(topNormalized); // Range [0,1]
+    const [index, setIndex] = useState(0);
+    const [newPosition, setNewPosition] = useState(new THREE.Vector3(...cameraPositions[index]));
 
+    const { camera } = useThree();
+    console.log(camera.position)
+    // console.log(newPosition);
+    window.cameraGlobal = camera;
     useFrame(()=>{
-        if(topNormalized < nextPoint ){
-            setTopNormalized(topNormalized + 0.01);
-        } else if (topNormalized > nextPoint) {
-            // setTopNormalized(topNormalized - 0.01);
-            setTopNormalized(nextPoint);
+        // console.log(newPosition)
+        if(group.current){
+            group.current.position.lerp(newPosition, 0.1);
         }
     });
-    
-    const [index, setIndex] = useState(0);
+
     const handleClick = useCallback((e)=>{
         const length = cameraPositions.length;
         setIndex( (index + 1) % length );
+        setNewPosition( new THREE.Vector3(...cameraPositions[index]) );
         console.log(index);
-        const indexNormalized = index / length;
-        setNextPoint(indexNormalized);
     });
-    const { camera } = useThree();
+
+    const group = useRef();
+    useEffect(()=>{
+        if(group.current){
+            // camera.add(group.current);
+            group.current.add(camera);
+        }
+    },[group.current])
     return (
-        <mesh position={[camera.position.x,camera.position.y,camera.position.z  - 0.5]} scale={[0.2,0.2,0.2]} onClick={handleClick}>
+        <group ref={group}>
+        <mesh position={[0,-0.3, -0.5]} scale={[0.1,0.1,0.1]} onClick={handleClick}>
             <planeBufferGeometry attach='geometry' args={[1,1]} />
-            <meshBasicMaterial attach='material' color='red' />
+            <meshBasicMaterial attach='material' color='red' side={THREE.DoubleSide} />
         </mesh>
+        </group>
     );
 }
 
@@ -83,9 +97,9 @@ export function Scene() {
     return(
         <>
         <ambientLight />
-        <Curve points={cameraPositions} top={topNormalized}>
+        {/* <Curve points={cameraPositions} top={topNormalized}>
             <Camera position={[0, 0, 0]} near={0.1} far={1000} fov={70} />
-        </Curve>
+        </Curve> */}
         <Suspense fallback={<Loading />}>
             <Pictures />
             <Arrows />
@@ -100,7 +114,7 @@ export default function App43(props) {
     return (
     <Canvas className="canvas" 
             style={{backgroundColor:'#000000'}}
-            // camera={{ position: [0, 0, 1], near: 0.01, far: 10000, fov:70 }}
+            camera={{ position: [0, 0, 0], near: 0.01, far: 10000, fov:70 }}
             onCreated={({ gl, camera }) => {
                 actions.init(camera);
                 // gl.gammaInput = true
@@ -111,3 +125,38 @@ export default function App43(props) {
     </Canvas>
     );
 }
+
+
+
+
+/** --------------- Old function -------------- */
+// export function Arrows({}) {
+//     const cameraPositions = useStore( state => state.cameraPositions );
+//     const [topNormalized, setTopNormalized] = useStore( state => [state.topNormalized,state.setTopNormalized]);
+//     const [nextPoint, setNextPoint] = useState(topNormalized); // Range [0,1]
+
+//     useFrame(()=>{
+//         if(topNormalized < nextPoint ){
+//             setTopNormalized(topNormalized + 0.01);
+//         } else if (topNormalized > nextPoint) {
+//             // setTopNormalized(topNormalized - 0.01);
+//             setTopNormalized(nextPoint);
+//         }
+//     });
+    
+//     const [index, setIndex] = useState(0);
+//     const handleClick = useCallback((e)=>{
+//         const length = cameraPositions.length;
+//         setIndex( (index + 1) % length );
+//         console.log(index);
+//         const indexNormalized = index / length;
+//         setNextPoint(indexNormalized);
+//     });
+//     const { camera } = useThree();
+//     return (
+//         <mesh position={[camera.position.x,camera.position.y,camera.position.z  - 0.5]} scale={[0.2,0.2,0.2]} onClick={handleClick}>
+//             <planeBufferGeometry attach='geometry' args={[1,1]} />
+//             <meshBasicMaterial attach='material' color='red' />
+//         </mesh>
+//     );
+// }
