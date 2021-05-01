daacee6df6bf85ae684678b868dc44e4fa162882
espisepi <sepinaco@gmail.com>
2021-05-02 01:53:10 +0200

    App49 Home: gl toneMapping modified

--- src/scenes/App49/App49.js
+++ src/scenes/App49/App49.js
@@ -91,7 +91,7 @@ export default function App49(props) {
     return (
     <>
     <div style={{overflow:'hidden'}}>
-    <Canvas className="canvas" style={{ backgroundColor:'#000', position:'fixed', width:'100%', height:'100vh', zIndex:'5'}} colorManagement>
+    <Canvas gl={{antialias: true}} onCreated={ ({gl}) => gl.toneMapping = 0 } className="canvas" style={{ backgroundColor:'#000', position:'fixed', width:'100%', height:'100vh', zIndex:'5'}} colorManagement>
         <Scene />
     </Canvas>
     <SectionsHtml />
