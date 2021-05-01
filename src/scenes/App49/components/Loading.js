2f00c0306145625aeed3b4ec2bb3b68a361cd907
espisepi <sepinaco@gmail.com>
2021-05-01 22:58:09 +0200

    App49 Home: audioSound in loading component

---
+++ src/scenes/App49/components/Loading.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react';
+import { useFrame } from 'react-three-fiber';
+import { Text } from 'drei';
+import { wellcomeSound, playAudio, stopAudio } from '../audios/index';
+
+export default function Loading() {
+    
+    const mesh = useRef(null);
+
+    useFrame(() => {
+        if( mesh ) {
+            mesh.current.rotation.y += 0.05;
+        }
+    })
+
+    useEffect(()=>{
+        playAudio(wellcomeSound, 1, true);
+        return () => {
+            stopAudio(wellcomeSound);
+        }
+    },[])
+
+    return (
+        <group >
+            <mesh ref={mesh}>
+                <boxBufferGeometry attach='geometry' args={[1,1,1]} />
+                <meshStandardMaterial attach='material' color='hotpink' />
+            </mesh>
+            <Text position={[0,-2,0]} fontSize={ 1.0 }>
+                Loading...
+            </Text>
+        </group>
+    );
+}
