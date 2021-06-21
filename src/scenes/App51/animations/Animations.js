546e32cffcedbf92e186d43ca1a59cdf1d74273c
espisepi <sepinaco@gmail.com>
2021-06-21 10:55:04 +0200

    App52 CatedralTourVR: separate animation for vr

--- src/scenes/App51/animations/Animations.js
+++ src/scenes/App51/animations/Animations.js
@@ -5,6 +5,14 @@ import useScroll from '../../../drei-espinaco/hooks/useScroll';
 import HelicopterCurveAnimation from './HelicopterCurveAnimation'
 import { pathTest } from '../paths'
 
+export function AnimationsVR() {
+    return (
+        <>
+        <HelicopterCurveAnimation pointsDefault={pathTest} />
+        </>
+    );
+}
+
 export default function Animations(){
 
     const top = useScroll('.section-one', '.section-eight');
